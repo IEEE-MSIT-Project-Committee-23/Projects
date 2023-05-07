e555a2404f86345dd69f3c9479d79d6c9933aac3
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-05-07 21:16:13 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          What the 👀🏥 fuck did you just 👆🏌 fucking 👉😂 say 😂 about 💦🤔 me, you little bitch? Ramesh the GOAT😂😭 
+          I’ll 🤧 have you know 🤔🤔 I 💰😜 graduated top 👆 of 💰🚋 my 👬 class in 🔥😩 my 😤 FIITJEE coaching 
+          centre and I’ve 💕 been 📽 involved in 〽 numerous 💰🔢 secret 😱 raids 👤🔛 on 🏿🔛 the 👏🌜 resonance 
+          coaching centre, and 👏👏 I have 🈶 AIR 🌟 300 💦💦 rank . I am ✅👏 trained 🏫🏻 in 👌📥 physics,chem
+           and 👏💰 mathematics and 👏👏 I’m the 💦👏 topper in my coaching centre. You 😂👈 are 🔢😫 nothing to 
+           💯 me 😍 but just 😤 another 👣👣 coaching centre student. 🏽 I 💲🙋 will wipe you 😵❌ the 👊 fuck 👉
+           🖕 out ⛵ with ❌ precision the 🌫👈 likes of 🅱 which ♀👏 has never ⛔❌ been 🔙 seen 👀 before 🎄 on
+            👇🏽 JEE, mark 🏼✌ my fucking 😡👉 words. 😢 You 👈😩 think you 🤖👆 can 💰 get 💪😕 away 😭 with 👏👏
+             saying 🙋🗣 that 💦 shit to 😵💦 me 👈 over facebook? 👯 Think again, fucker. ➡🍆 As we 🤔 speak I 
+             😎🚟 am 👏👈 contacting 📞👤 my 🏾👨 coaching centre for 💰🎁 new 🆕🗽 study 📓📓 materials so 🗿 
+             you 🤞 better ↗👍 prepare 💋👏 for the 👏🍝 storm, maggot. The 👏 storm that wipes out 💯 the pathetic
+              😂😭 little 😤👧 thing you call your 💁🤕 life. 💘😱 You’re 😀😄 fucking 👌👏 dead, 🤣 kid. I can be
+               😎🅱 anywhere, anytime, ⏰ and 👥 I 💕 can 💦🔫 out rank you 👉😍 in 🍆👏 over seven hundred ways, 
+               and 👏 that’s just 🏌👏 with 👏 JEE. Not 🚫 only 🕦👏 am 👏 I extensively trained in 👌👏 KVPY, 
+               but 🍑 I have 🈶😤 access to all 👀 the ♂💴 FIITJEE secret 😱 study 📚 materials and I 👁 will 🙈
+                use 😎 it ❗💯 to 💦🎯 its 📞💰 full 🈵 extent to 🎲💦 wipe 🤤 your miserable rank off the ☸ 
+                face of this 🏼 topper list, you little 🔜 shit. 💩💖 If 👩👩 only 😂 you ♀🏃 could 🔒 have 🌈🈶
+                 known 💫💫 what unholy 🙏 retribution your 🏳👅 little “clever” rank was 💦😨 about to 💰👉 
+                 bring ⬅⬅ down ⬇🏽 upon you, maybe you 🚫👉 would have 🈶♂ held ✊✊ your 💝 fucking 👉😈 hand.
+                  ✋ But 💁😈 you ✌😩 couldn’t, you didn’t, 😊😑 and 💰🙇 now 👎 you’re 💸 paying the 🔥🍗
+                   price, you goddamn idiot. 🤥😂 I will ❓👏 shit fury 😡😡 all ✋ over you and you 👃 will 
+                   😳💯 drown in it. You’re 🤧 fucking dead, 🐩🙍 kiddo. 💲🔥
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
